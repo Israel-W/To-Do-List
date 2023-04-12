f8ca7e2a2c9f12fad3cbe0a2772ed88e509c7bc0
Israel-W <isracss48@gmail.com>
2023-04-12 16:42:16 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const formEl = document.querySelector(".form");
+
+const inputEl = document.querySelector("input")
+
+const ulEl = document.querySelector(".list")
+
+let list = JSON.parse(localStorage.getItem("list"));
+
+
+
+formEl.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    toDoList()
+});
+
+function toDoList(task){
+    let newTask = inputEl.value;
+    if(task){
+        newTask = task.name
+    }
+    
+
+
+  const liEl = document.createElement("li");
+  if(task && task.checked){
+    liEl.classList.add("checked") 
+  }
+  liEl.innerText = newTask;
+  ulEl.appendChild(liEl)
+  inputEl.value = ""
+  const checkBtnel = document.createElement("div")
+  checkBtnel.innerHTML = `
+  <i class="fa-solid fa-square-check"> 
+  `;
+  liEl.appendChild(checkBtnel);
+  const trashBtnEl = document.createElement("div")
+  trashBtnEl.innerHTML = `
+  <i class="fa-solid fa-trash"></i>
+  `;
+  liEl.appendChild(trashBtnEl);
+
+  checkBtnel.addEventListener("click", ()=>{
+      liEl.classList.toggle("checked")
+  })
+
+  trashBtnEl.addEventListener("click", ()=>{
+      liEl.remove(); 
+      updateLocalStorage();
+  })
+  updateLocalStorage()
+}
+
+function updateLocalStorage(){
+    const liEls = document.querySelector("li")
+    list = []
+    liEls.forEach(liEl=>{
+        list.push({
+            name: liEl.innerText,
+            checked: liEl.classList.contains(checked)
+        })
+    })
+    localStorage.setItem("list", JSON.stringify(list))
+}
